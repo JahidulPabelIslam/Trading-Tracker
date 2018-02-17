df8b8906d745a99f10610e8681cb79946a90ee48
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-02-17 17:32:18 +0000

    Add debug param for quick dev testing for js/css files

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Trading Tracker</title>
+
+        <?php if (isset($_GET["debug"])):?>
+            <link href="/assets/css/third-party/bootstrap.min.css" rel="stylesheet" title="style" media="all" type="text/css">
+            <link href="/assets/css/trading-tracker/style.css" rel="stylesheet" title="style" media="all" type="text/css">
+        <?php else: ?>
+            <!-- Complied CSS File of all CSS Files -->
+            <link href="assets/css/main.min.css" rel="stylesheet" title="style" media="all" type="text/css">
+        <?php endif; ?>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Trading Tracker</a>
+        </nav>
+
+        <main role="main" class="container">
+
+        </main>
+
+        <footer class="footer">
+            <div class="container">
+                <p>&copy; <a href="https://jahidulpabelislam.000webhostapp.com/">Jahidul Pabel Islam</a> 2018</p>
+                <p>Team <a href="https://www.instagram.com/myriad.official/">#Myriad</a> / Team <a href="https://www.jkmt.co.uk/">#JKMT</a></p>
+            </div>
+        </footer>
+
+        <?php if (isset($_GET["debug"])):?>
+            <script src="/assets/js/third-party/jquery-3.2.1.min.js" type="application/javascript"></script>
+            <script src="/assets/js/third-party/popper.min.js" type="application/javascript"></script>
+            <script src="/assets/js/third-party/bootstrap.min.js" type="application/javascript"></script>
+        <?php else: ?>
+            <!-- Complied JavaScript File of all JavaScript Files -->
+            <script src="/assets/js/main.min.js" type="application/javascript"></script>
+        <?php endif; ?>
+    </body>
+</html>
