0ad75dd5e496010d2e54d3d5210f09b348b69db2
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-02-18 20:58:53 +0000

    Calculate total pips made / lost

--- assets/js/trading-tracker/app.js
+++ assets/js/trading-tracker/app.js
@@ -1,4 +1,5 @@
 var app = angular.module("TradingTrackerApp", []);
+
 app.controller("ctrl", function ($scope, $filter) {
     $scope.newtrade = [];
     $scope.types = ["Sell", "Buy"];
@@ -17,6 +18,27 @@ app.controller("ctrl", function ($scope, $filter) {
 
     $scope.addTrade = function() {
         $scope.newtrade.date = $filter('date')($scope.newtrade.date, "dd/MM/yyyy");
+
+        var entryprice = parseFloat($scope.newtrade.entryprice, 10);
+        var exitprice = parseFloat($scope.newtrade.exitprice, 10);
+
+        var pips = 0;
+        if ($scope.newtrade.type === "Buy") {
+            pips = new Decimal(exitprice).minus(entryprice);
+        } else {
+            pips = new Decimal(entryprice).minus(exitprice);
+        }
+
+        pips = parseFloat(pips, 10);
+
+        var name = $scope.newtrade.name.toLowerCase();
+
+        if (name.includes("jpy") || name.includes("xau")) {
+            $scope.newtrade.pips = pips / 0.01;
+        } else {
+            $scope.newtrade.pips = pips / 0.0001;
+        }
+
         $scope.trades.push($scope.newtrade);
         $("#addTrade").modal("hide");
         $scope.newtrade = [];
