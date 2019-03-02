86cfc8dac9e34008ed64b06ca2e9f2ee137ab689
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2019-03-02 16:39:45 +0000

    Add favicons

--- index.php
+++ index.php
@@ -64,6 +64,29 @@
         }
         ?>
 
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicons/apple-touch-icon-57x57.png?v=1" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicons/apple-touch-icon-60x60.png?v=1" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicons/apple-touch-icon-72x72.png?v=1" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicons/apple-touch-icon-76x76.png?v=1" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicons/apple-touch-icon-114x114.png?v=1" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicons/apple-touch-icon-120x120.png?v=1" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicons/apple-touch-icon-144x144.png?v=1" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicons/apple-touch-icon-152x152.png?v=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon-180x180.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="194x194" href="/assets/favicons/favicon-194x194.png?v=1" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicons/android-chrome-192x192.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/assets/favicons/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg?v=1" color="#ffd700" />
+        <link rel="shortcut icon" href="favicon.ico?v=1" />
+        <meta name="apple-mobile-web-app-title" content="<?php echo $appName; ?>" />
+        <meta name="application-name" content="<?php echo $appName; ?>" />
+        <meta name="msapplication-TileColor" content="#ffd700" />
+        <meta name="msapplication-TileImage" content="/assets/favicons/mstile-144x144.png?v=1" />
+        <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml?v=1" />
+        <meta name="theme-color" content="#343a40" />
+
         <?php
         if (isset($_GET["debug"])) {
             ?>
