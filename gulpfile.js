ecb0a3f8b6cc75053307e1488472ea934eaea2cb
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2019-03-18 22:58:03 +0000

    Make version text into a link to github release. Fixes #3

--- gulpfile.js
+++ gulpfile.js
@@ -77,18 +77,27 @@ gulp.task("watch", function() {
 });
 
 gulp.task("store-version", function() {
+    var githubBaseUrl = "https://github.com/jahidulpabelislam/trading-tracker/";
     var fileName = "assets/version.txt";
+    var versionText = "";
+
+    var errorCallback = function(err) {
+        if (err) {
+            console.log(err);
+        }
+    };
 
     // Try to get current branch name
     exec("git branch | grep \\* | cut -d ' ' -f2", function(branchNameErr, branchName, branchNameStderr) {
-
         /*
          * If name found store in text file
          * If current branch if master we used use tags (As most likely this is in production environment)
          * Else it is one of dev branches so display branch name
          */
         if (branchName && branchName !== "null" && branchName.trim() !== "master") {
-            fs.writeFile(fileName, branchName.trim());
+            branchName = branchName.trim();
+            versionText = `<a href="${githubBaseUrl}tree/${branchName}/" class="" target="_blank">${branchName}</a>`;
+            fs.writeFile(fileName, versionText, errorCallback);
         }
         else {
             // Else just log errors & try to store latest tag name string in text file
@@ -98,10 +107,10 @@ gulp.task("store-version", function() {
 
             // Try and get the latest tag on current branch
             exec("git describe --abbrev=0 --tags\n", function(tagNameErr, tagName, tagNameStderr) {
-
                 // If found store in text file
                 if (tagName && tagName !== "null") {
-                    fs.writeFile(fileName, tagName.trim());
+                    tagName = tagName.trim();
+                    versionText = `<a href="${githubBaseUrl}releases/tag/${tagName}/" class="" target="_blank">${tagName}</a>`;
                 }
                 else {
                     // Else log any errors
@@ -111,14 +120,14 @@ gulp.task("store-version", function() {
 
                     // Else drop back to branch name if exists else remove version value from file
                     if (branchName && branchName !== "null") {
-                        fs.writeFile(fileName, branchName.trim());
-                    }
-                    else {
-                        fs.writeFile(fileName, "");
+                        branchName = branchName.trim();
+                        versionText = `<a href="${githubBaseUrl}tree/${branchName}/" class="link-styled" target="_blank">${branchName}</a>`;
                     }
                 }
             });
         }
+
+        fs.writeFile(fileName, versionText, errorCallback);
     });
 });
 
